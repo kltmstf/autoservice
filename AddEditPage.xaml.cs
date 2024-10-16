8d0184f1310201e0f0ea708d38edc764dadf06a8
УАТ <@STUD231-1>
2024-10-16 11:00:59 +0500

    лаба 6

--- AddEditPage.xaml.cs
+++ AddEditPage.xaml.cs
@@ -20,9 +20,53 @@ namespace lab3
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        
+        
+        
+        private Service _currentService = new Service();
+        public AddEditPage(Service SelectedService)
         {
             InitializeComponent();
+
+            if (SelectedService != null)
+                _currentService = SelectedService;
+
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("Укажите название услуги");
+            if (_currentService.Cost <= 0)
+                errors.AppendLine("Укажите стоимость услуги");
+            if (_currentService.Discount < 0)
+                errors.AppendLine("Укажите скидку");
+            if (_currentService.Discount > 100)
+                errors.AppendLine("Укажите скидку");
+            if (_currentService.Discount == null)
+                errors.AppendLine("Укажите скидку");
+            if (string.IsNullOrWhiteSpace(_currentService.Duration))
+                errors.AppendLine("Укажите длительность услуги");
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentService.ID == 0)
+                Lapitskaya_autoserviceEntities.GetContext().Service.Add(_currentService);
+            try
+            {
+                Lapitskaya_autoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -75,10 +75,6 @@ namespace lab3
                 ServiceListView.ItemsSource = currentService.OrderBy(p => p.Cost).ToList();
             }
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new AddEditPage());
-        }
 
         private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -98,6 +94,25 @@ namespace lab3
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             UpdateService();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                Lapitskaya_autoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = Lapitskaya_autoserviceEntities.GetContext().Service.ToList();
+            }
         }
     }
 }
